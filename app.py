fb29e05f70149b685a70a7be2553e0c1ea2fa06a
Serhii Klimushyn <seroga2801@gmail.com>
2022-02-09 14:15:08 +0200

    OK

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from database import app
+from flask import Flask
+
+from database import db
 from expenses import exp
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testtask.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 app.register_blueprint(exp, url_prefix='/')
 
--- database.py
+++ database.py
@@ -1,12 +1,7 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testtask.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
 
